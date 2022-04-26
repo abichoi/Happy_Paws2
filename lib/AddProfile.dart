fb4e2b21221b5665c60fdc310a7d1b991f05f381
Abi Choi <abichoiabichoi@gmail.com>
2022-04-26 17:46:48 +0100

    Able to upload pet profile Able to show on homepage as horizontal list

--- lib/AddProfile.dart
+++ lib/AddProfile.dart
@@ -1,8 +1,13 @@
+// import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+// import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart'as firebase_storage;
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+// import 'package:firebase_core/firebase_core.dart';
 // import 'dropdownsex.dart';
 // import 'datepicker.dart';
 
@@ -16,6 +21,13 @@ class AddProfileFormWidget extends StatefulWidget {
 
 class AddProfileFormWidgetState extends State<AddProfileFormWidget> {
   // late DatabaseReference _ref;
+  // late String _uploadedFileURL;
+  late String returnURL;
+  firebase_storage.FirebaseStorage storage = firebase_storage.FirebaseStorage.instance;
+  File? _photo;
+  // late Future<PickedPhoto?> _photo = Future.value(null);
+  final ImagePicker _picker = ImagePicker();
+
   final _formKey = GlobalKey<FormState>();
   final _name = TextEditingController();
   final _species = TextEditingController();
@@ -36,151 +48,234 @@ class AddProfileFormWidgetState extends State<AddProfileFormWidget> {
     //form widget
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: _name,
-            decoration: const InputDecoration(
-              hintText: 'Enter the Name',
-              labelText: 'Name',
-              labelStyle: TextStyle(fontSize: 18),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                child: _photo == null ? const Text('No Image Showing') : Image.file(_photo!)
             ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the name';
-              }
-              return null;
-            },
-          ),
-
-
-          TextFormField(
-            controller: _species,
-            decoration: const InputDecoration(
-              hintText: 'Enter the species',
-              labelText: 'Species',
-              labelStyle: TextStyle(fontSize: 18),
+            // FutureBuilder<_photo?>(
+            //   future: _photo,
+            //   builder: (context, snap) {
+            //     if (snap.hasData) {
+            //       return Container(
+            //         child: Image.file(
+            //           File(snap.data!.path),
+            //           fit: BoxFit.contain,
+            //         ),
+            //         color: Colors.blue,
+            //       );
+            //     }
+            //     return Container(
+            //       height: 200.0,
+            //       color: Colors.blue,
+            //     );
+            //   },
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                    onPressed: () => imgFromCamera(),
+                    icon: const Icon(Icons.camera),
+                    label: const Text('camera')),
+                ElevatedButton.icon(
+                    onPressed: () => imgFromGallery(),
+                    icon: const Icon(Icons.library_add),
+                    label: const Text('Gallery')),
+              ],
             ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the species';
-              }
-              return null;
-            },
-          ),
-          // TextFormField(
-          //   controller: _sex,
-          //   decoration: const InputDecoration(
-          //     hintText: 'Enter the sex',
-          //     labelText: 'Species',
-          //     labelStyle:TextStyle(fontSize: 18),
-          //   ),
-          //   validator: (String? value) {
-          //     if (value == null || value.isEmpty) {
-          //       return 'Please enter the sex (F/M)';
-          //     }
-          //     return null;
-          //   },
-          // ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
-            child: Text(
-                'Sex', style: TextStyle(fontSize: 18, color: Colors.black54)),
-          ),
-          Container(
-            height: 40.0,
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-            decoration: BoxDecoration(
-                color: const Color(0xFFF2F3F5),
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.black12)
-            ),
-            // child: const SexDropDownWidget(),
-            child: DropdownButton<String>(
-              value: _showvalue,
-              icon: const Icon(Icons.arrow_drop_down_outlined),
-              elevation: 16,
-              style: const TextStyle(color: Colors.black),
-              borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-              dropdownColor: const Color(0xFFF2F3F5),
-              iconEnabledColor: const Color(0xFFB8C1CC),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _showvalue = newValue!;
-                  _sex = _showvalue;
-                });
+            TextFormField(
+              controller: _name,
+              decoration: const InputDecoration(
+                hintText: 'Enter the Name',
+                labelText: 'Name',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the name';
+                }
+                return null;
               },
-              items: _dropdownlistValueSex
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
             ),
-          ),
 
 
-          TextFormField(
-            controller: _breed,
-            decoration: const InputDecoration(
-              hintText: 'Enter the breed',
-              labelText: 'Breed',
-              labelStyle: TextStyle(fontSize: 18),
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the address';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _color,
-            decoration: const InputDecoration(
-              hintText: 'Enter the color',
-              labelText: 'Color',
-              labelStyle: TextStyle(fontSize: 18),
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the email';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _dob,
-            decoration: const InputDecoration(
-              hintText: 'Enter the date of birth (dd/mm/yyyy)',
-              labelText: 'Date of Birth',
-              labelStyle: TextStyle(fontSize: 18),
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter the date of birth (dd/mm/yyyy)';
-              }
-              return null;
-            },
-          ),
-          // const Padding(
-          //   padding: EdgeInsets.fromLTRB(0,10,0,5),
-          //   child: Text('Date of Birth', style: TextStyle(fontSize:18, color: Colors.black54)),
-          // ),
-          // Container(height: 50, child: DatePicker(),),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                savePetProfile();
+            TextFormField(
+              controller: _species,
+              decoration: const InputDecoration(
+                hintText: 'Enter the species',
+                labelText: 'Species',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the species';
+                }
+                return null;
               },
-              child: const Text('Submit'),
             ),
-          ),
-        ],
+            // TextFormField(
+            //   controller: _sex,
+            //   decoration: const InputDecoration(
+            //     hintText: 'Enter the sex',
+            //     labelText: 'Species',
+            //     labelStyle:TextStyle(fontSize: 18),
+            //   ),
+            //   validator: (String? value) {
+            //     if (value == null || value.isEmpty) {
+            //       return 'Please enter the sex (F/M)';
+            //     }
+            //     return null;
+            //   },
+            // ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+              child: Text(
+                  'Sex', style: TextStyle(fontSize: 18, color: Colors.black54)),
+            ),
+            Container(
+              height: 40.0,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              decoration: BoxDecoration(
+                  color: const Color(0xFFF2F3F5),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black12)
+              ),
+              // child: const SexDropDownWidget(),
+              child: DropdownButton<String>(
+                value: _showvalue,
+                icon: const Icon(Icons.arrow_drop_down_outlined),
+                elevation: 16,
+                style: const TextStyle(color: Colors.black),
+                borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                dropdownColor: const Color(0xFFF2F3F5),
+                iconEnabledColor: const Color(0xFFB8C1CC),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _showvalue = newValue!;
+                    _sex = _showvalue;
+                  });
+                },
+                items: _dropdownlistValueSex
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+
+
+            TextFormField(
+              controller: _breed,
+              decoration: const InputDecoration(
+                hintText: 'Enter the breed',
+                labelText: 'Breed',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the address';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _color,
+              decoration: const InputDecoration(
+                hintText: 'Enter the color',
+                labelText: 'Color',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the email';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _dob,
+              decoration: const InputDecoration(
+                hintText: 'Enter the date of birth (dd/mm/yyyy)',
+                labelText: 'Date of Birth',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the date of birth (dd/mm/yyyy)';
+                }
+                return null;
+              },
+            ),
+            // const Padding(
+            //   padding: EdgeInsets.fromLTRB(0,10,0,5),
+            //   child: Text('Date of Birth', style: TextStyle(fontSize:18, color: Colors.black54)),
+            // ),
+            // Container(height: 50, child: DatePicker(),),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    savePetProfile();
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future imgFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+        uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future imgFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+        uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future uploadFile() async {
+    if (_photo == null) return;
+    final fileName = path.basename(_photo!.path);
+    const destination = 'user-images';
+
+    try {
+      final reference = firebase_storage.FirebaseStorage.instance
+          .ref(destination)
+          .child(fileName);
+      await reference.putFile(_photo!);
+      print('File Uploaded');
+      await reference.getDownloadURL().then((fileURL) {
+        returnURL =  fileURL;
+      });
+    } catch (e) {
+      print('error');
+    }
   }
 
 
@@ -192,18 +287,19 @@ class AddProfileFormWidgetState extends State<AddProfileFormWidget> {
     String color = _color.text;
     String dob = _dob.text;
 
-    // Map<String, String> petprofile = {
-    //   'name': name,
-    //   'species': species,
-    //   'sex': sex,
-    //   'breed': breed,
-    //   'color': color,
-    //   'dob': dob,
-    // };
+    Map<String, String> _petprofile = {
+      'name': name,
+      'species': species,
+      'sex': sex,
+      'breed': breed,
+      'color': color,
+      'dob': dob,
+      'img': returnURL
+    };
 
     FirebaseFirestore.instance
         .collection('Pet_Profile')
-        .add({'name': name, 'species': species, 'sex': sex, 'breed': breed, 'color': color, 'dob': dob});
+        .add(_petprofile);
   }
 
 }
--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boxicons/flutter_boxicons.dart';
 import 'AddContact.dart';
 import 'AddProfile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -15,8 +16,8 @@ class _HomePageWidget extends StatefulWidget {
 }
 
 class _HomePageWidgetState extends State<_HomePageWidget>{
-  final title1_Home = new Text('Pet Profiles', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),);
-  final title2_Home = new Text('Contact', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),);
+  final title1_Home = const Text('Pet Profiles', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),);
+  final title2_Home = const Text('Contact', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),);
   final margin = const EdgeInsets.only(bottom: 10.0, right: 10.0, left: 10.0);
   @override
   Widget build(BuildContext context) {
@@ -25,51 +26,51 @@ class _HomePageWidgetState extends State<_HomePageWidget>{
     return Column(
         children: <Widget>[
           Container(
-            width: width,
+            // width: width,
             height: height/3,
-            color: Colors.green,
-            child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Container (
-                    child: Row (
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: < Widget> [
-                          Text('Pet Profile', style: new TextStyle(fontSize: 30.0, fontWeight:FontWeight.bold  ),),
+            // color: Colors.green,
+            child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row (
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: < Widget> [
+                            const Text('Pet Profile', style: TextStyle(fontSize: 30.0, fontWeight:FontWeight.bold  ),),
+                            IconButton(
+                              icon: const Icon(Icons.add),
+                              color: Colors.black,
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (_) => const AddProfilePage()),);
+                                },
+                              tooltip: 'Add Pet Profile',),
+                          ]
+                      ),
+                      Expanded(
+                        child: _PetProfileList(),
+                      ),
+                    ]
+                ),
 
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            color: Colors.black,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (_) => AddProfilePage()),
-                              );
-                            },
-                            tooltip: 'Add Pet Profile',),
-                        ]
-                    )
-                )
-            ),
           ),
           Expanded(
               child: Container(
                 width: width,
                 color: Colors.blueGrey,
                 child: Padding(
-                    padding: EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(8.0),
                     child: Row (
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: < Widget> [
-                          Text('Contact', style: new TextStyle(fontSize: 30.0, fontWeight:FontWeight.bold  ),),
+                          const Text('Contact', style: TextStyle(fontSize: 30.0, fontWeight:FontWeight.bold  ),),
                           IconButton(
-                            icon: Icon(Icons.add),
+                            icon: const Icon(Icons.add),
                             color: Colors.black,
                             onPressed: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (_) => addcontactpage()),
+                                MaterialPageRoute(builder: (_) => const addcontactpage()),
                               );
                             },
                             tooltip: 'Add Pet Profile',),
@@ -112,10 +113,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   ];
 
   final _PageOption = [
-    _HomePageWidget(),
-    Text('Appointment', style: optionStyle,),
-    Text('Storage',style: optionStyle,),
-    Text('Shop', style: optionStyle, ),
+    const _HomePageWidget(),
+    const Text('Appointment', style: optionStyle,),
+    const Text('Storage',style: optionStyle,),
+    const Text('Shop', style: optionStyle, ),
   ];
 
   final _colorOptions = [
@@ -183,6 +184,52 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     );
   }
 }
+
+  class _PetProfileList extends StatelessWidget {
+  final db = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    body: StreamBuilder<QuerySnapshot>(
+      stream: db.collection('Pet_Profile').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot _pet = snapshot.data!.docs[index];
+              return Card(
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    debugPrint('Card tapped.');
+                  },
+                  child: Container(
+                    width: 150,
+                    child: Column(
+                      children:<Widget>[
+                        Image.network(_pet.get("img")),
+                        Text(_pet.get("name")),
+                      ]
+                    ),
+                  ),
+                ),
+
+              );
+            },
+          );
+        }},
+    ),
+  );}
+}
+
+
 
 class HomePage extends StatelessWidget{
   const HomePage({Key? key}) : super(key: key);
